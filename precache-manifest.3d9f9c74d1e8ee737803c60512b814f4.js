8e64cc6a232128f83ebec777e60f74611edf882b
aadi30thakur <aadi30.thakur@gmail.com>
2020-10-09 00:50:22 +0530

    Updates

---
+++ precache-manifest.3d9f9c74d1e8ee737803c60512b814f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1322d1432445be2de5121f424f2d91f",
+    "url": "/GIF-link-gen/index.html"
+  },
+  {
+    "revision": "b4e41af58e7d2e361e75",
+    "url": "/GIF-link-gen/static/css/main.1eb63f87.chunk.css"
+  },
+  {
+    "revision": "0ff5b49874fbdc0d9d67",
+    "url": "/GIF-link-gen/static/js/2.2e5b90e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GIF-link-gen/static/js/2.2e5b90e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e41af58e7d2e361e75",
+    "url": "/GIF-link-gen/static/js/main.a940a9bf.chunk.js"
+  },
+  {
+    "revision": "c6bbf2ecb6a7907e3a64",
+    "url": "/GIF-link-gen/static/js/runtime-main.3d5c2921.js"
+  }
+]);
